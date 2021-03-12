4c1b18c0ac933ba301dfdfb2933656b526c059f9
Rebirth3125 <79037935+Rebirth3125@users.noreply.github.com>
2021-03-12 17:59:37 +0600

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -4,13 +4,144 @@
 #include <string.h>
 int main()
 {
-    float x = 0, y = 0, r = 0, per = 0, S = 0;
-    printf_s("Enter the values\n");
-    scanf_s("%f", &x);
-    scanf_s("%f", &y);
-    scanf_s("%f", &r);
-    per = 2 * P * r;
-    S = P * r * r;
-    printf_s("Perimetr=%f\n", per);
-    printf_s("Square=%f\n", S);
+    double x, y, r;
+    char str[100];
+    int flag = 0;
+    char circle[7] = "circle";
+    char* point = str;
+    char* start_point;
+    char* last_point = str;
+    char* end;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (isalpha(*last_point) != 0)
+        last_point++;
+
+    if ((last_point - point) <= 6) {
+        if (strncmp(str, circle, 6) == 0)
+            flag = 1;
+    } else if (strncmp(str, circle, last_point - point) == 0)
+        flag = 1;
+    if (flag == 1) {
+        flag = 0;
+        point = last_point;
+        start_point = point;
+
+        while (*start_point != 10) {
+            if (*start_point == '(') {
+                point = start_point;
+                flag = 1;
+                break;
+            }
+            start_point++;
+        }
+
+        if (flag == 0) {
+            printf("ERROR: expected 'circle' (\n");
+            return 0;
+        }
+
+        while (isdigit(*point) == 0) // Первое число
+        {
+            if ((*point == ' ') || (*point == '('))
+                point++;
+            else {
+                printf("ERROR X\n");
+                return 0;
+            }
+        }
+
+        if (isdigit(*point) != 0) {
+            x = strtod(point, &end);
+            point = end;
+            printf("x=%f\n", x);
+        }
+
+        while (isdigit(*point) == 0) // Второе число
+        {
+            if (*point == ' ')
+                point++;
+            else {
+                printf("ERROR Y\n");
+                return 0;
+            }
+        }
+
+        if (isdigit(*point) != 0) {
+            y = strtod(point, &end);
+            point = end;
+            printf("y= %f\n", y);
+        }
+
+        start_point = point;
+        flag = 0;
+
+        while (*start_point != 10) {
+            if ((*start_point != ' ') && (*start_point != ',')) {
+                printf("ERROR ,\n");
+                return 0;
+            } else if (*start_point == ',') {
+                point = start_point;
+                flag = 1;
+                break;
+            } else
+                start_point++;
+        }
+
+        if (flag == 0)
+            printf("ERROR ,");
+
+        while (isdigit(*point) == 0) // Третье число
+        {
+            if ((*point == ' ') || (*point == ','))
+                point++;
+            else {
+                printf("ERROR Radius\n");
+                return 0;
+            }
+        }
+
+        if (isdigit(*point) != 0) {
+            r = strtod(point, &end);
+            point = end;
+            printf("r=%f\n", r);
+        }
+
+        flag = 0;
+        start_point = point;
+
+        while (*start_point != 10) {
+            if ((*start_point != ' ') && (*start_point != ')')) {
+                printf("ERROR )\n");
+                return 0;
+            } else if (*start_point == ')') {
+                point = start_point;
+                flag = 1;
+                break;
+            } else
+                start_point++;
+        }
+
+        if (flag == 0) {
+            printf("ERROR )\n");
+            return 0;
+        }
+
+        point++;
+
+        while (*point != 10) {
+            if (*point != ' ') {
+                printf("ERROR: no items expected\n");
+                return 0;
+            } else
+                point++;
+        }
+        printf("data entered correctly\n");
+        return 0;
+    }
+
+    else
+        printf("ERROR: expected 'circle'\n");
+    return 0;
 }

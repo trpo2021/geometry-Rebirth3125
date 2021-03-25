cc34ca4216ec52078aab548dd5b265fd15db53c6
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-03-26 00:51:15 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <string>
+#include <vector>
+
+#include "check.h"
+#include "calculate.h"
+
+int main()
+{
+  double x, y, r;
+  char str[100];
+  int flag = 0;
+  char circle[7] = "circle";
+  char* point = str;
+  char* start_point;
+  char* last_point = str;
+  char* end;
+  void check(point = str, start_point, last_point = str,flag);
+  double Perimetr(figureName, number);
+  double Square(figureName, number);
+  
+}

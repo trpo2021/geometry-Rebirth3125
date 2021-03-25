a8a1ceb638e9203d9e2935ad22148eb70d876963
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-03-26 00:53:40 +0600

    Create check.h

---
+++ check.h
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#progma once
+
+void check(char* point = str,  char* start_point, char* last_point = str, int flag)

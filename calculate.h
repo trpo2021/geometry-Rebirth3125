1dd4bf3a16326e93604734df6ee421018476d189
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-03-29 14:39:02 +0600

    Create calculate.h

---
+++ calculate.h
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+using namespace std;
+
+double Perimetr(string figureName, vector<double> number);
+double Square(string figureName, vector<double> number);

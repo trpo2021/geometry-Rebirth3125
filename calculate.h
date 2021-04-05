46c2395345b7bff7f07c07b336d25ab5922eff44
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-04-05 12:45:05 +0600

    Delete calculate.h

--- calculate.h
+++
@@ -1,9 +0,0 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <list>
-
-double Perimetr(char figureName, list<double> number);
-double Square(char figureName, list<double> number);

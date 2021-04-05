69de7fc547df93ef1dfba7cb5f3418b613dbdafb
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-04-05 12:45:12 +0600

    Update check.h

--- check.h
+++ check.h
@@ -1,8 +1,4 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #pragma once
 
-void check(char* point = str,  char* start_point, char* last_point = str, int flag)
+int name_verification(char* input_arr);
+int checking_parameters_figure(char* input_arr);

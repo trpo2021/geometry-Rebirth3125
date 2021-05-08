2274b97c916d19c030e2ad0b275ad31656d34eec
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-05-08 18:29:56 +0600

    create test.c

---
+++ thirdparty/test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ctest.h"
+#include "libgeometry/check.h"
+#include "stdbool.h"
+
+CTEST(input, word_chek)
+{
+    char* str = "circle(1 2,3)";
+    char* ukazatel = str;
+    bool result = name_verification(str);
+    ASSERT_TRUE(result);
+}
+CTEST(input, word_chek2)
+{
+    char* str = "Circle(1 2,3)";
+    char* ukazatel = str;
+    bool result = name_verification(str);
+    ASSERT_FALSE(result);
+}
+CTEST(input, word_chek3)
+{
+    char* str = "circle(1 2,3)";
+    char* ukazatel = str;
+    bool result = checking_parameters_figure(str);
+    ASSERT_TRUE(result);
+}
+CTEST(input, word_chek4)
+{
+    char* str = "Circle(3 3 3)";
+    char* ukazatel = str;
+    bool result = checking_parameters_figure(str);
+    ASSERT_FALSE(result);
+}
+CTEST(input, word_chek5)
+{
+    char* str = "Circle( 2,3";
+    char* ukazatel = str;
+    bool result = checking_parameters_figure(str);
+    ASSERT_FALSE(result);
+}

64742c061d00a013ac1fce93018961e35ba3465d
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-05-15 21:43:50 +0600

    fix errors

--- thirdparty/test.c
+++ thirdparty/test.c
@@ -7,35 +7,30 @@
 CTEST(input, word_chek)
 {
     char* str = "circle(1 2,3)";
-    char* ukazatel = str;
     bool result = name_verification(str);
     ASSERT_TRUE(result);
 }
 CTEST(input, word_chek2)
 {
     char* str = "Circle(1 2,3)";
-    char* ukazatel = str;
     bool result = name_verification(str);
     ASSERT_FALSE(result);
 }
 CTEST(input, word_chek3)
 {
     char* str = "circle(1 2,3)";
-    char* ukazatel = str;
     bool result = checking_parameters_figure(str);
     ASSERT_TRUE(result);
 }
 CTEST(input, word_chek4)
 {
     char* str = "Circle(3 3 3)";
-    char* ukazatel = str;
     bool result = checking_parameters_figure(str);
     ASSERT_FALSE(result);
 }
 CTEST(input, word_chek5)
 {
     char* str = "Circle( 2,3";
-    char* ukazatel = str;
     bool result = checking_parameters_figure(str);
     ASSERT_FALSE(result);
 }

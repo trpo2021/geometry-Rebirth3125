75498f02a2c3c53518c31f40692d49082b264dec
Ilya Musorov <79037935+Rebirth3125@users.noreply.github.com>
2021-05-15 21:55:52 +0600

    formatting

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -9,19 +9,18 @@
 
 int main()
 {
-    float radius = 0,radius2=0;
-    float area = 0, perimetr = 0, area2=0, perimetr2=0;
+    float radius = 0, radius2 = 0;
+    float area = 0, perimetr = 0, area2 = 0, perimetr2 = 0;
     char r_arr[N], r_arr2[N];
-    char input_arr[N] = {0}, input_arr2[N]={0};
+    char input_arr[N] = {0}, input_arr2[N] = {0};
     FILE* fptr = NULL;
     FILE* fptr2 = NULL;
     char* ukazat1 = input_arr;
     char* ukazat2 = input_arr2;
 
-
     fptr = fopen("input.txt", "r");
     fptr2 = fopen("input2.txt", "r");
-    if (!fptr||!fptr2) {
+    if (!fptr || !fptr2) {
         printf("Can`t open the file\n");
     } else {
         int i = 0;
@@ -119,19 +118,21 @@ int main()
             printf("perimeter2 = %.3f\n", perimetr2);
         }
     }
-    char *x1_s = (ukazat1 +7);
-    char *x2_s = (ukazat2 +7);
-    char *y1_s = (ukazat1 +9);
-    char *y2_s = (ukazat2 +9);
+    char* x1_s = (ukazat1 + 7);
+    char* x2_s = (ukazat2 + 7);
+    char* y1_s = (ukazat1 + 9);
+    char* y2_s = (ukazat2 + 9);
     int x1 = atoi(x1_s);
     int x2 = atoi(x2_s);
     int y1 = atoi(y1_s);
     int y2 = atoi(y2_s);
 
-    if(radius-radius2<sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))&&sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))<radius+radius2)
-      printf(" figure intersect ");
+    if (radius - radius2 < sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+        && sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+                < radius + radius2)
+        printf(" figure intersect ");
     else
-      printf(" figure not intersect ");
+        printf(" figure not intersect ");
 
     return 0;
-  }
+}
